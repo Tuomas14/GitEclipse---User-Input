5fc50a520682f6d7fde94442e67829eb7b01c8f5
Tuomas <tuomas.valtonen@student.hamk.fi>
2023-08-31 14:55:27 +0300

    First commit
    Publishing the first commit on the assigment "User Input in CodeRunner". The assignment was to "Ask the name of the user and print it."

--- src/GitEclipse_UserInput.java
+++ src/GitEclipse_UserInput.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class GitEclipse_UserInput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	Scanner in = new Scanner(System.in);
+
+	String answer;
+	
+	System.out.println("What is your name?");
+	answer = in.nextLine();
+		
+	System.out.println("Your name is " + answer + ".");
+		
+		
+		
+		
+		
+		
 	}
 
 }
